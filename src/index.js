aa48fed7fe13fd233c56cf8fdb5121564560c2a3
Quannichan <tranminhquan130804@gmail.com>
2024-11-15 00:06:20 +0700

    init port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = 6000;
 const {Route} = require("../router/route");
 
 app.set('trust proxy', true);
